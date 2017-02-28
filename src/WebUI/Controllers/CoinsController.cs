3ea262bee8440096bf87c05c079431b04791c63b
Richard Case <richard.case@outlook.com>
2017-02-28 16:51:43 +0000

    Updated webui to use ip address for redis connection

--- src/WebUI/Controllers/CoinsController.cs
+++ src/WebUI/Controllers/CoinsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -56,18 +57,42 @@ namespace WebUI.Controllers
 
         private ConnectionMultiplexer OpenConnection(string cacheAddress, string cachePassword)
         {
+            string redisIp = GetIPFromHostName(cacheAddress);
+
             ConfigurationOptions redisConfig = new ConfigurationOptions
             {
                 EndPoints =
                 {
-                    { cacheAddress }
+                    { redisIp }
                 },
                 AbortOnConnectFail = false,
                 Password = cachePassword,
                 Ssl = true
             };
+            redisConfig.CertificateValidation += (sender, cert, chain, errors) => true;
 
             return ConnectionMultiplexer.Connect(redisConfig);
+        }
+
+        private string GetIPFromHostName(string hostName)
+        {
+
+            string[] s = hostName.Split(':');
+            string host = s[0];
+            int port = Convert.ToInt32(s[1]);
+
+            if (IsIpAddress(host))
+            {
+                return hostName;
+            }
+
+            var ip = Dns.GetHostEntryAsync(host).GetAwaiter().GetResult();
+            return $"{ip.AddressList.First(x => IsIpAddress(x.ToString()))}:{port}";
+        }
+
+        private static bool IsIpAddress(string host)
+        {
+            return Regex.IsMatch(host, @"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b");
         }
     }
 }
